176acc38a5dc419c3e68c0db6a156bc36e92cb7c
Tanvir_Mahmud <161886678+TanvirMahmud-1@users.noreply.github.com>
2025-07-20 02:42:30 +0600

    Add files via upload

---
+++ A_Square_of_Rectangles.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int l1, b1, l2, b2, l3, b3;
+        cin >> l1 >> b1 >> l2 >> b2 >> l3 >> b3;
+
+        if (l1 + l2 + l3 == b1 && b1 == b2 && b2 == b3)
+            cout << "YES" << endl;
+        else if (l2 + l3 == l1 && b2 == b3 && b1 + b2 == l1)
+            cout << "YES" << endl;
+        else if (b1 + b2 + b3 == l1 && l1 == l2 && l2 == l3)
+            cout << "Yes" << endl;
+        else if (b2 + b3 == b1 && l2 == l3 && l1 + l2 == b1)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+}
